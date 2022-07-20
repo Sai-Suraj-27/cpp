14b836d8764e190c503ad02d6a466543dbc0a5c9
Sai Suraj <ce20b031@iittp.ac.in>
2022-07-20 18:21:16 +0530

    Basic stack.

--- stack.cpp/1.cpp
+++ stack.cpp/1.cpp
@@ -15,17 +15,55 @@ using namespace std;
     return equal(s.begin(), s.end(), s.rbegin());
 }*/
 
+void printstack(stack<string> st)
+{
+    while(!st.empty())
+    {
+        cout << st.top() << "\n";
+        st.pop();
+    }
+}
+
 
 int main() {
 	// your code goes here.
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     
+    // Last in first out.
+    stack<string> st;
+    // push
+    st.push("garlic");
+	st.push("Cabbage");
+    st.push("Potato");
+    st.push("Apple");
+
+    printstack(st);
+    cout << "\n";
+    
+    st.pop();
+    printstack(st);
+    cout << "\n";
+    
+    st.push("onion");
+    st.push("skfdl");
+    st.push("ajbsl");
+
+    printstack(st);
+    cout << endl;
+
+    st.pop();
+    st.pop();
+	
+    printstack(st);
+    cout << "\n";
+
+
+    // Used in DFS of a graph.
+    // Used in compiler syntax for checking matching brackets, braces.
+    // Undo Mechanisms in text editors etc...
+
     
     
-    
-	
-	
-	
-	return 0;
+    return 0;
 }
