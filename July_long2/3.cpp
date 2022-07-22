a86649a0f954acbf52f4af63cd74b6439cc7d416
Sai Suraj <ce20b031@iittp.ac.in>
2022-07-22 18:31:17 +0530

    July long 2

---
+++ July_long2/3.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)x.size()
+#define all(x) (x).begin(), (x).end()
+#define F(i, a, n) for (ll i = a; i < n; i++)
+#define B(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define ll long long int
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define vll vector<ll>
+ll mod = 1e9 + 7;
+using namespace std;
+
+
+int main()
+{
+    // your code goes here.
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
