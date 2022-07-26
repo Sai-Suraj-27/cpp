f471a8565210dbf5528f626a25b166cceda4d80f
Sai Suraj <ce20b031@iittp.ac.in>
2022-07-27 01:05:41 +0530

    shivendra

---
+++ extra.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    vector<int> v;
+    int n;
+    cin >> n;
+    int k;
+    vector<int> v2;
+    int minIndex = -1;
+    for(int i=0;i<n;i++)
+    {
+        cin >> k;
+        v.push_back(k);
+    }
+    int minimum = INT_MAX;
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        minimum = v[i];
+        minIndex = i;
+        for(j=0;j<n;j++)
+        {
+            if(v[j] < minimum)
+            {
+                minIndex = j;
+                minimum = v[j];
+            }
+        }
+        cout << minIndex << " ";
+        v[minIndex] = INT_MAX;
+    }
+
+
+    
+    return 0;
+}
