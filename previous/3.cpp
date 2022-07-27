0556b3daa2014624494888e1b883ff10bef9846e
Sai Suraj <ce20b031@iittp.ac.in>
2022-07-27 11:03:12 +0530

    solved 3

---
+++ previous/3.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#define sz(x) (int)x.size()
+#define all(x) (x).begin(), (x).end()
+#define F(i,a,n) for(ll i=a;i<n;i++)
+#define B(i,a,n) for(ll i=n-1;i>=a;i--)
+#define ll long long int
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define vll vector<ll>
+ll mod = 1e9 + 7;
+using namespace std;
+
+/* inline bool isPalindrome(const string& s) {
+    return equal(s.begin(), s.end(), s.rbegin());
+}*/
+
+
+int main()
+{
+	// your code goes here.
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+  
+    ll t;
+    cin >> t;
+    
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+
+        vll v;
+        ll i,j,k;
+
+        for(i=0;i<n;i++)
+        {
+            cin >> k;
+            v.pb(k);
+        }
+        ll c = 0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(v[i] == n)
+            {
+                swap(v[i],v[i + 1]);
+                c++;
+            }
+        }
+        for(int i=n-1;i>0;i--)
+        {
+            if(v[i] == 1)
+            {
+                swap(v[i],v[i - 1]);
+                c++;
+            }
+        }
+        cout << c << endl;
+    }
+
+	return 0;
+}
