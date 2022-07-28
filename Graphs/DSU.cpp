a1719a00d34cc5c360c0d2432f216ed7c8fa41be
Sai Suraj <ce20b031@iittp.ac.in>
2022-07-28 18:12:19 +0530

    DSU

---
+++ Graphs/DSU.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    // for group related questions, DSU is very useful.
+    // Finding connected components.
+    // DSU is a Data strucutre
+    // 3 Main fns of DSU are:   1. Make fn (To add an independent node to a network or to create new independent nodes),
+    // 2. Find function (Gives the parent of the group or node)
+    // 3. Union function (keep a,b into 1 group, If a&b are groups then they can be combined)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
