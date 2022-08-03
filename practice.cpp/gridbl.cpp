d0b10997cc0879da25f12cbb89534483b41de362
Sai Suraj <ce20b031@iittp.ac.in>
2022-08-03 11:54:30 +0530

    1st one - div2

---
+++ practice.cpp/gridbl.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define sz(x) (int)x.size()
+#define all(x) (x).begin(), (x).end()
+#define F(i,a,n) for(ll i=a;i<n;i++)
+#define B(i,a,n) for(ll i=n-1;i>=a;i--)
+#define ll long long int
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define vll vector<ll>
+ll mod = 1e9 + 7;
+using namespace std;
+
+/* inline bool isPalindrome(const string& s) {
+    return equal(s.begin(), s.end(), s.rbegin());
+}*/
+
+
+int main()
+{
+	// your code goes here.
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+  
+    ll t;
+    cin >> t;
+    
+    while(t--)
+    {
+        ll n,m;
+        cin >> n >> m;
+
+        ll k = min(m,n);
+        ll l = max(m,n);
+        if(k==1)
+        {
+            cout << l << endl;
+        }
+        else
+        {
+           cout << m*n - 4*((m/2)*(n/2)) << endl;
+        }
+        
+    }
+	
+	return 0;
+}
