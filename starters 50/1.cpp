c0a9ee293ec094eea5f4c9bf9a89c3a3ae444061
Sai Suraj <ce20b031@iittp.ac.in>
2022-08-03 20:49:02 +0530

    completed 1 qn

---
+++ starters 50/1.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+#define sz(x) (int)x.size()
+#define all(x) (x).begin(), (x).end()
+#define F(i,a,n) for(ll i=a;i<n;i++)
+#define B(i,a,n) for(ll i=n-1;i>=a;i--)
+#define ll long long int
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define vll vector<ll>
+ll mod = 1e9 + 7;
+using namespace std;
+
+/* inline bool isPalindrome(const string& s) {
+    return equal(s.begin(), s.end(), s.rbegin());
+}*/
+
+template<typename T>
+bool allEqual(std::vector<T> const &v) {
+    if (v.size() == 0) {
+        return false;
+    }
+    return std::equal(v.begin() + 1, v.end(), v.begin());
+}
+
+ll no_uniq(vector<ll> v)
+{
+    sort(all(v));
+    return distance(v.begin(),unique(v.begin(), v.end()));
+}
+
+
+void solve(ll &extra, ll n, vll &v, vll &v1)
+{
+    ll num = 1 + extra;
+
+    v.pb(min(num,n));
+    v1[min(num,n)]--;
+    extra -= min(num,n) - 1;
+
+    return;
+}
+
+
+int main()
+{
+	// your code goes here.
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+  
+    ll t;
+    cin >> t;
+    
+    while(t--)
+    {
+        ll n,x;
+        cin >> n >> x;
+        vll v;
+        vll v1(n+1);
+        ll i,j,k;
+        for(i=1;i<=n;i++)
+        {
+            v1[i]++;
+        }
+        ll extra = x-n;
+        
+        if(x<n)
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            while(extra != 0)
+            {
+                solve(extra,n,v,v1);
+            }
+            for(i=1;i<=n;i++)
+            {   
+                if(v1[i] != 0)
+                {
+                    v.pb(i);
+                }
+            }
+            for(i=0;i<sz(v);i++)
+            {
+                cout << v[i] << " ";
+            }
+            
+        }
+        cout << endl;
+    }
+    
+	return 0;
+}
