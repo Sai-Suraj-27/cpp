a276003e19d1e8b637670c8a720c55d8260d22cf
Sai Suraj <ce20b031@iittp.ac.in>
2022-08-05 19:40:51 +0530

    tried meeting place but the idea is not correct for some cases, The correct way is to use a binary search based on time, like from 1 to 10000000 seconds...we should check.min time

--- NPTEL course/The_Meeting_Place_Cannot_Be_Changed.cpp
+++ NPTEL course/The_Meeting_Place_Cannot_Be_Changed.cpp
@@ -43,8 +43,52 @@ int main()
     
     while(t--)
     {
-        
-        
+        ll n;
+        cin >> n;
+
+        vll x;
+        vll v;
+
+        ll i,j,k;
+        ll minx = INT_MAX:
+        ll v_minx = 0, v_maxx = 0; 
+        ll maxx = INT_MIN;
+        for(i=0;i<n;i++)
+        {
+            cin >> k;
+            x.pb(k);
+        }
+        for(i=0;i<n;i++)
+        {
+            cin >> k;
+            v.pb(k);
+        }
+
+        for(i=0;i<n;i++)
+        {
+            if(x[i] > maxx)
+            {
+                maxx = x[i];
+                v_maxx = v[i];
+            }
+            if(x[i] < minx)
+            {
+                minx = x[i];
+                v_minx = v[i];
+            }
+            ll dist = maxx - minx;
+            if(v_minx != v_maxx)
+            {
+                l
+            }
+
+
+
+
+        }
+
+
+
         
         
         
