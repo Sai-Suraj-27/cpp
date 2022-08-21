2e5c76347f91559b75d09cea0e91fa48942d6825
Sai Suraj <ce20b031@iittp.ac.in>
2022-08-21 15:06:35 +0530

    Merge sort

--- new.cpp/mergesort.cpp
+++ new.cpp/mergesort.cpp
@@ -67,32 +67,103 @@ auto it = std::remove_if(str.begin(), str.end(), [](char const &c) {
 // std::rotate(nums.begin(),nums.begin() + n-(k%n), nums.end()); to rotate vector to right k steps.
 // if we want an element at index 'Z' to become 1st element after rotations we replace n-(k&n) with z;
 
+/*
 void pairsort(vll &a, vll &b, ll n)
 {
     vector<pair<ll,ll>> v(n);
 
     for(int i=0;i<n;i++)
     {
-        v[i] = {a[i],b[i]};
+        v[s] = i] = {a[i],b[i]};
     }
     sort(all(v));
 
     for(int i=0;i<n;i++)
     {
-        a[i] = v[i].first;
-        b[i] = v[i].second;
+        a[i] = v[s] = i].first;
+        b[i] = v[s] = i].second;
     }
 
 }
+*/
 
+
+void merge(vll &v, ll s, ll e)
+{
+    ll i;
+    ll mid = s + (e-s)/2;
+
+    ll l1 = mid - s + 1;
+    ll l2 = e - mid;
+
+    vll v1,v2;
+    for(i=s;i<=mid;i++)
+    {
+        v1.pb(v[i]);
+    }
+    cout << v[i] << " ";
+    for(i=mid+1;i<e;i++)
+    {
+        v2.pb(v[i]);
+    }
+
+    // making a final sorted array from these 2 v1,v2 sorted arrays.
+
+    for(auto i: v1) cout << i << " ";
+    cout << "\n";
+
+    for(auto i: v2) cout << i << " ";
+    cout << "\n";
+
+
+    i=0;
+    ll j=0;
+    while(i<sz(v1) or j<sz(v2))
+    {
+        if(i==sz(v1))
+        {
+            v[s] = (v2[j]);
+            j++;
+            s++;
+        }
+        else if(j==sz(v2))
+        {
+            v[s] = (v1[i]);
+            i++;
+            s++;
+        }
+        else if(v1[i] <= v2[j])
+        {
+            v[s] = (v1[i]);
+            i++;
+            s++;
+        }
+        else if(v2[j] < v1[i])
+        {
+            v[s] = (v2[j]);
+            j++;
+            s++;
+        }
+    }
+    return;
+}
 
 void mergesort(vll &v, ll s, ll e)
 {
-    
+    if(s>=e)
+    {
+        return;
+    }
 
+    ll mid = (s + e)/2;
 
+    mergesort(v,s,mid);
+    mergesort(v,mid+1,e);
 
+    merge(v,s,e);
 
+    // for(auto i: v) cout << i << " ";
+    // cout << "\n";
 
 }
 
@@ -105,38 +176,27 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    vll v;
+    
     ll n;
     cin >> n;
-
+    vll v(n);
     ll i,j,k;
     
     for(i=0;i<n;i++)
     {
         cin >> k;
-        v.pb(k);
+        v[i] = k;
     }
 
     mergesort(v,0,n-1);
 
-    
+    for(auto i: v) cout << i << " ";
 
 
 
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
