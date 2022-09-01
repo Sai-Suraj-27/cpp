ef6d601952f519c6a9e0c05df2994d8c4c4203ee
Sai Suraj <ce20b031@iittp.ac.in>
2022-09-01 15:11:10 +0530

    solved 1 prev qn and tried 2nd qn

--- fore/d1.cpp
+++ fore/d1.cpp
@@ -1,11 +1,61 @@
 #include<bits/stdc++.h>
+#define ll  long long
+#define vll vector<ll>
 using namespace std;
-# define ll long long
-# define vll vector<ll>
-# define pb push_back
+
+
+class Node
+{
+    Node* links[26];
+    bool flag = false;
+    int cp = 0;
+
+    bool containskey(char ch)
+    {
+        return links[ch - 'a'] != NULL;
+    }
+    void put(char ch, Node* new1)
+    {
+        links[ch-'a'] = new1;
+    }
+    void next(char ch)
+    {
+        return links[ch-'a'];
+    }
+
+    void declarecp(int n)
+    {
+        cp = n;
+    }
 
 
 
+}
+
+
+class trie
+{
+    Node* root;
+    trie()
+    {
+        root = new Node();
+    }
+
+    void index(string word, )
+    {
+
+    }
+
+    void query(string word)
+    {
+
+    }
+
+    void update(string word)
+
+
+
+}
 
 
 
@@ -14,6 +64,20 @@ using namespace std;
 
 int main()
 {
+   ll n;
+   cin >> n;
+
+   while(n--)
+   {
+     char ch;
+     cin >> ch;
+
+     if(ch == 'I')
+     {
+        
+     }
+        
+   }
     
 
 
--- fore/d2.cpp
+++ fore/d2.cpp
@@ -1,9 +1,8 @@
 #include<bits/stdc++.h>
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
 using namespace std;
-# define ll long long
-# define vll vector<ll>
-# define pb push_back
-
 
 
 
@@ -14,10 +13,41 @@ using namespace std;
 
 int main()
 {
+    ll n;
+    cin >> n;
 
+    vll v;
+    ll i,j,k;
 
+    for(i=0;i<n;i++)
+    {
+        cin >> k;
+        v.pb(k-(i+1));
+    }
 
+    // sigma (v[i]-i) - x shoud be min;
+// 10 8 6 4 2 0 -2 -4 -6 -8 -10
+// med = 0;
 
+    sort(v.begin(),v.end());
+
+    ll med;
+    if(n%2==1)
+    {
+        med = v[n/2];
+    }
+    else
+    {
+        med = v[n/2] + v[n/2 - 1];
+    }
+
+    ll mint = 0;
+
+    for(i=0;i<v.size();i++)
+    {
+        mint += abs(v[i]-med);
+    }
+    cout << mint << endl;
 
 
 
