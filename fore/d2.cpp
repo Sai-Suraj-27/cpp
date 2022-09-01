3b4137034ea3d7cd3be971c89d49926bac112abd
Sai Suraj <ce20b031@iittp.ac.in>
2022-09-01 21:39:50 +0530

    completed nptel 2nd qn

--- fore/d2.cpp
+++ fore/d2.cpp
@@ -1,81 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<bool>vis;
 
+int mf=0,mof=0;
 
-
-
-int bfs(int i,vector<int>adj[], vector<int> &vis){
-
+void dfs(int i, vector<int> adj[], int eFlip, int oFlip, int level, vector<int>v, vector<int> d)
+{
 
     vis[i]=1;
-    queue<int> q;
 
-    q.push(i);
-    int level=0;
+    if(level%2==0 && eFlip%2!=0)v[i]=1-v[i];
+    else if(level%2!=0 && oFlip%2!=0)v[i]=1-v[i];
 
-    while(!q.empty())
-    {
-        int m = q.size();
-        for(int k=0;k<m;k++)
-        {
-            int cur = q.front();
-            q.pop();
 
-            for(auto itr:adj[cur])
-            {
-                if(!vis[itr])
-                {
-                    q.push(itr);
-                    vis[itr]=1;
-                }
-            }
+    if(v[i]!=d[i]){
+
+        if(level%2==0){
+            eFlip++;
+            mf++;
         }
-
-        level++;
+        else{
+            oFlip++;
+            mof++;
+        }
         
     }
 
-    return level;
+    
+
+    for(auto itr:adj[i])
+    {
+        if(!vis[itr])
+        {
+            dfs(itr,adj,eFlip,oFlip,level+1,v,d);
+        }
+    }
 }
 
-int main(){
-
+int main()
+{
     int n;
-    cin>>n;
-
-    vector<int>parent(n+1,0);
-
-    for(int i=1;i<=n;i++)
-    {
-        cin>>parent[i];
-    }
+    
+    cin >> n;
 
     vector<int>adj[n+1];
 
-    vector<int>v;
-
-    for(int i=1;i<=n;i++){
-
-        if(parent[i]!=-1)
-        adj[parent[i]].push_back(i);
-        else{
-            v.push_back(i);
-        }
-
-        
-    }
-    vector<int> vis;
     vis.resize(n+1,0);
 
-    int maxi =0;
-    
+    for(int i=1;i<=n-1;i++)
+    {
+        int x,y;
+        cin>>x>>y;
 
-    for(auto i:v){
-        if(!vis[i]){
-           int cur =  bfs(i,adj,vis);
-           maxi = max(cur,maxi);
-        }
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
-    cout<<maxi;
+
+    vector<int>v(n+1);
+    vector<int>d(n+1);
+
+    for(int i=1;i<=n;i++)
+    {
+        cin>>v[i];
+    }
+
+    for(int i=1;i<=n;i++)
+    {
+        cin>>d[i];
+    }
+
+    dfs(1,adj,0,0,0,v,d);
+
+    cout<< mof + mf;
+
+
 }
