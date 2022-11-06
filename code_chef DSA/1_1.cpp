4c69adf397fa75ec43e16f148dfcee634f2039b0
Sai Suraj <ce20b031@iittp.ac.in>
2022-11-06 18:53:58 +0530

    solved 1st problems dsa_CC

---
+++ code_chef DSA/1_1.cpp
@@ -0,0 +1,49 @@
+# include<bits/stdc++.h>
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    ll t;
+    cin>> t;
+    while(t--)
+    {
+        ll h,x,y;
+        cin >> h>>x>>y;
+
+        if(x>y)
+        {
+            cout << 1 << "\n";
+        }
+        else
+        {
+            cout << 0 << "\n";
+        }
+
+    }
+    return 0;
+
+
+
+
+
+
+
+    return 0;
+}
