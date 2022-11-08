be7d7333872f320da6c469889b6fde750a393d79
Sai Suraj <ce20b031@iittp.ac.in>
2022-11-08 18:18:53 +0530

    solved 6th problem DSA-CC>

--- code_chef DSA/1_6.cpp
+++ code_chef DSA/1_6.cpp
@@ -28,8 +28,14 @@ int main()
         double p,x,y,z;
         cin >> p >> x >> y >> z;
 
-        
+        if(z==1)
+        {
+            p += y*p/100.0;
+        }
+        else
+            p -= x*p/100.0;
 
+        cout << fixed << setprecision(8) << p << endl;
 
 
 
